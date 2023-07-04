a8ec43a6849e4e50832eff11f08aefa62ab1e92c
supermanisme2002 <bthanh2001@gmail.com>
2023-07-04 17:30:55 +0700

    remove component import wrong

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import EventsManager from "./components/EventsManager/EventsManager";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App mx-auto max-w-6xl text-center my-8">
         <h1 className="font-semibold text-2xl">
           React - The Road To Enterprise
         </h1>
-        <EventsManager />
       </div>
     </QueryClientProvider>
   );
